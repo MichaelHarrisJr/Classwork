300d20be388592c6a70b99b8bf32ff00c578a408
Michael Harris <31981399+xshull123@users.noreply.github.com>
2019-01-27 23:23:01 -0500

    Create EvenOdd.c

---
+++ CSC-292/EvenOdd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{ 
+int x;
+printf("Enter any number ");
+scanf("%d",x);
+printf("\n");
+if(x%2==0)
+printf("then it is a even number ");
+else
+printf("then it is a odd number ");
+
+return 0;
+
+}
