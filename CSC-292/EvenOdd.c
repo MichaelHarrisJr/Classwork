023183a286273b4b9b5f73e2cbabe3d2bb8bfe90
Michael Harris <31981399+michaelharrisjr95@users.noreply.github.com>
2019-01-28 14:05:56 -0500

    Update EvenOdd.c

--- CSC-292/EvenOdd.c
+++ CSC-292/EvenOdd.c
@@ -3,9 +3,9 @@ int main()
 { 
 int x;
 printf("Enter any number ");
-scanf("%d",x);
+scanf("%d",&x);
 printf("\n");
-if(x%2==0)
+if(x % 2 == 0)
 printf("then it is a even number ");
 else
 printf("then it is a odd number ");
