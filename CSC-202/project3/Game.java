6fbc6ed3f0f6b2a659dab8d490225bd210048b33
MichaelHarrisJr <m.f.harris99819@spartans.nsu.edu>
2019-06-26 10:35:34 -0400

    remove project3

--- CSC-202/project3/Game.java
+++
@@ -1,8 +0,0 @@
-package project3;
-
-public interface Game {  // interface
-    public abstract String getWinner();  // toss coin, if tie
-    public abstract String getLoser();
-    public abstract int    getWinnerScore();
-    public abstract int    getLoserScore();
-}  // interface Game
